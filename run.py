4d11c6e7073edae3a58eb397ab90ba6daa3eda6c
norman <norman@archT450>
2024-10-31 10:49:22 +0200

    Idkw commit

--- run.py
+++ run.py
@@ -9,13 +9,15 @@ from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
 from ryu.lib.packet import ipv4
 from ryu.lib.packet import arp
-from ryu.lib.packet import icmp
-from ryu.lib.packet import tcp
 import ryu.topology.event as topo_event
 
 import networkx as nx
+import time
 from time import sleep
-import sys
+
+IDLE_TIMEOUT = 30
+HARD_TIMEOUT = 30
+
 
 class Mapper():
     """ map IPs, macs, port, and datapaths to each other """
@@ -46,19 +48,76 @@ class Mapper():
         """ map an IP to a dpid """
         self._ip2dpid[ip] = (dpid, port)
 
+
 class Balancer(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
-    IDLE_TIMEOUT = 30
-    HARD_TIMEOUT = 30
 
     def __init__(self, *args, **kwargs):
         super(Balancer, self).__init__(*args, **kwargs)
         self.G = nx.DiGraph()
         self.M = Mapper()
 
-    def _lb_choose_path(self, paths):
-        # TODO: do a round robin
-        return paths[0]
+    def _arp_handler(self, dp, in_port, pkt):
+        """ handle ARP PacketIn's """
+        ofproto = dp.ofproto
+        parser = dp.ofproto_parser
+        h = pkt.get_protocol(arp.arp)
+
+        if self.M.ip2dpid(h.dst_ip) is None:
+            self.logger.info("dst %s is not known. flooding...", h.dst_ip)
+            msg = parser.OFPPacketOut(datapath=dp,
+                                      buffer_id=ofproto.OFP_NO_BUFFER,
+                                      in_port=in_port,
+                                      actions=[parser.OFPActionOutput(
+                                          ofproto.OFPP_ALL)],
+                                      data=pkt.data)
+            dp.send_msg(msg)
+            self.logger.info("")
+            return
+
+        _dpid, _port = self.M.ip2dpid(h.dst_ip)
+        _dp = get_datapath(self, _dpid)
+        self.logger.info("send ARP message through %s port %s", _dpid,
+                         _port)
+        msg = parser.OFPPacketOut(datapath=_dp,
+                                  buffer_id=ofproto.OFP_NO_BUFFER,
+                                  in_port=ofproto.OFPP_CONTROLLER,
+                                  actions=[parser.OFPActionOutput(
+                                      _port)],
+                                  data=pkt.data)
+        _dp.send_msg(msg)
+        self.logger.info("")
+
+    def _ip_handler(self, dp, in_port, pkt):
+        """ handle IP PacketIn's """
+        ofproto = dp.ofproto
+        parser = dp.ofproto_parser
+        h = pkt.get_protocol(ipv4.ipv4)
+
+        if self.M.ip2dpid(h.dst) is None:
+            self.logger.info("Mapper does not know where %s connected", h.dst)
+            return
+
+        paths = self._lb_find_paths(h.src, h.dst)
+        self.logger.info("paths from %s to %s: %s", h.src, h.dst, paths)
+        path = self._lb_choose_path(paths)
+        self.logger.info("path %s is choosen", path)
+
+        match = parser.OFPMatch(
+            eth_type=ether_types.ETH_TYPE_IP,
+            ipv4_src=h.src,
+            ipv4_dst=h.dst)
+        self._lb_install_path(path, h.src, h.dst, match)
+
+        # reprocess this packet again
+        msg = parser.OFPPacketOut(datapath=dp,
+                                  buffer_id=ofproto.OFP_NO_BUFFER,
+                                  in_port=in_port,
+                                  actions=[parser.OFPActionOutput(
+                                      ofproto.OFPP_TABLE)],
+                                  data=pkt.data)
+        dp.send_msg(msg)
+        self.logger.info("")
 
     def _lb_find_paths(self, src_ip, dst_ip):
         node1, _ = self.M.ip2dpid(src_ip)
@@ -67,87 +126,85 @@ class Balancer(app_manager.RyuApp):
         paths = list(nx.all_shortest_paths(self.G, node1, node2))
         return paths
 
-    # TODO: this mess is not right and needs refactoring....
-    def _lb_install_path(self, path, src_ip, dst_ip):
-        edges = list(zip(path, path[1:]))   # [(s1, s2)...]
+    def _lb_choose_path(self, paths):
+        # TODO: do a round robin
+        return paths[time.time_ns() % len(paths)]
 
-        # prepare the packetout message
+    # TODO: this mess is not right and needs refactoring....
+    def _lb_install_path(self, path, src_ip, dst_ip, match):
+        edges = list(zip(path, path[1:]))   # [(s1, s2)...]
 
         for e in edges:
             dpid = e[0]
             port = self.G.edges[e]["port"]
-            # send a flow mod to this dp
             dp = get_datapath(self, dpid)
-            ofproto = dp.ofproto
             parser = dp.ofproto_parser
-            match = parser.OFPMatch(eth_type=0x0800, ipv4_src=src_ip,
-                                    ipv4_dst=dst_ip)
             actions = [parser.OFPActionOutput(port)]
 
-            self.logger.info("addflow dp %s match ipv4_src %s ipv4_dst %s output port %d", dpid, src_ip, dst_ip, port)
-            self.add_flow(dp, 10, match, actions)
+            self.logger.info("addflow dp %s match %s output port %d",
+                             dpid, match, port)
+            self.add_flow(dp, prio=10, idle=IDLE_TIMEOUT,
+                          hard=HARD_TIMEOUT, match=match, actions=actions)
 
         # this handles the last datapath, or if the path contains only a
         # single datapath.
         dpid = path[-1]
         port = self.M.ip2dpid(dst_ip)[1]
-        self.logger.info("addflow dp %s (last dp) match ipv4_src %s ipv4_dst %s output port %d", dpid, src_ip, dst_ip, port)
+        self.logger.info("addflow dp %s match %s output port %d",
+                         dpid, match, port)
         dp = get_datapath(self, dpid)
-        ofproto = dp.ofproto
         parser = dp.ofproto_parser
-        match = parser.OFPMatch(eth_type=0x0800, ipv4_src=src_ip,
-                                ipv4_dst=dst_ip)
         actions = [parser.OFPActionOutput(port)]
-        self.add_flow(dp, 10, match, actions)
+        self.add_flow(dp, prio=10, idle=IDLE_TIMEOUT,
+                      hard=HARD_TIMEOUT, match=match, actions=actions)
+        # this sleep prevents a problem that i can not explain...
+        sleep(0.01)
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def _switch_features_handler(self, ev):
         """
         """
-        datapath = ev.msg.datapath
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
+        dp = ev.msg.datapath
+        ofproto = dp.ofproto
+        parser = dp.ofproto_parser
 
         match = parser.OFPMatch()
         actions = [parser.OFPActionOutput(
             ofproto.OFPP_CONTROLLER,
             ofproto.OFPCML_NO_BUFFER
-            )]
+        )]
 
-        self.add_flow(datapath, 0, match, actions)
+        self.add_flow(dp, prio=0, match=match, actions=actions)
 
-        # TODO: make ARP packet OUTPUT NORMAL for now
-        match = parser.OFPMatch(
-                eth_type=0x0806
-                )
-        actions = [parser.OFPActionOutput(ofproto.OFPP_NORMAL)]
-        self.add_flow(datapath, 10, match, actions)
+    def add_flow(self, dp, tbl=0, prio=0, idle=0, hard=0, match=None,
+                 actions=[]):
+        ofproto = dp.ofproto
+        parser = dp.ofproto_parser
 
-    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
+        if dp is None:
+            self.logger.error("add_flow(): no datapath was given")
+            return
 
-#         inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
-#                                              actions)]
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
 
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_WRITE_ACTIONS,
-                                      actions)]
+        mod = parser.OFPFlowMod(datapath=dp, priority=prio,
+                                idle_timeout=idle, hard_timeout=hard,
+                                match=match, instructions=inst)
+        dp.send_msg(mod)
 
-        if buffer_id:
-            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
-                                    priority=priority, match=match,
-                                    instructions=inst)
-        else:
-            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-                                    match=match, instructions=inst)
-        datapath.send_msg(mod)
+#         if buffer_id:
+#             mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+#                                     priority=priority, match=match,
+#                                     instructions=inst)
+#         else:
+#             mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+#                                     match=match, instructions=inst)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
         msg = ev.msg
         dp = msg.datapath
-        ofproto = dp.ofproto
-        parser = dp.ofproto_parser
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocol(ethernet.ethernet)
         ip = pkt.get_protocol(ipv4.ipv4)
@@ -157,53 +214,35 @@ class Balancer(app_manager.RyuApp):
         if eth.ethertype == ether_types.ETH_TYPE_LLDP:
             return
 
-        self.logger.info(
-                "dp %s PacketIn reason %s in_port %s ipv4_src %s ipv4_dst %s",
-                dp.id, msg.reason, in_port, ip.src, ip.dst)
+        if eth.ethertype == ether_types.ETH_TYPE_ARP:
+            h = pkt.get_protocol(arp.arp)
+            self.logger.info(
+                "dp %s PacketIn Type ARP opcode %d in_port %s src_ip %s dst_ip %s",
+                dp.id, h.opcode, in_port, h.src_ip, h.dst_ip)
 
-        # learn the addresses in this messages.
-        # if ARP Requset:
-        # * we map the ip to the mac, ip to datapath and port.
-        # * if the request address is in the table,
-        #
-        # if ARP Reply:
-        # * map the ip to the mac, ip to datapath and port
-        #
-        # if IP packet...
-        #
+            self.M.map_ip2mac(h.src_ip, h.src_mac)
+            self.logger.info("Mapped %s to %s", h.src_ip, h.src_mac)
+            if self.M.ip2dpid(h.src_ip) is None:
+                self.M.map_ip2dpid(h.src_ip, dp.id, in_port)
+                self.logger.info("Mapped %s to dp %s port %s",
+                                 h.src_ip, dp.id, in_port)
 
-        self.M.map_ip2mac(ip.src, eth.src)
-        self.logger.info("Mapped %s to %s", ip.src, eth.src)
-        self.M.map_ip2mac(ip.dst, eth.dst)
-        self.logger.info("Mapped %s to %s", ip.dst, eth.dst)
-        self.M.map_ip2dpid(ip.src, dp.id, in_port)
-        self.logger.info("Mapped %s to dp %s port %s", ip.src, dp.id, in_port)
+            self._arp_handler(dp, in_port, pkt)
+            return
 
-        # find a path through the network for this flow, multiple paths
-        # are available, they must be utilized.
-        paths = self._lb_find_paths(ip.src, ip.dst)
-        self.logger.info("dp %s: paths from %s to %s: %s", dp.id,
-                         ip.src, ip.dst, paths)
-        path = self._lb_choose_path(paths)
-        self.logger.info("path %s is choosen", path)
-        self._lb_install_path(path, ip.src, ip.dst)
-
-        msg = parser.OFPPacketOut(datapath=dp,
-                                  buffer_id=ofproto.OFP_NO_BUFFER,
-                                  in_port=ofproto.OFPP_CONTROLLER,
-                                  actions=[parser.OFPActionOutput(ofproto.OFPP_TABLE)],
-                                  data=pkt.data)
-        dp.send_msg(msg)
+        if eth.ethertype == ether_types.ETH_TYPE_IP:
+            h = pkt.get_protocol(ipv4.ipv4)
+            self.logger.info(
+                "dp %s PacketIn type IP in_port %s ipv4_src %s ipv4_dst %s",
+                dp.id, in_port, ip.src, ip.dst)
+            self._ip_handler(dp, in_port, pkt)
+            return
 
     @set_ev_cls(topo_event.EventSwitchEnter)
     def _switch_enter_handler(self, ev):
         """
         handle new switches (datapaths)
         """
-#         self.logger.debug("memebers of %s: ", ev)
-#         for a in inspect.getmembers(ev):
-#             self.logger.debug("\t %s\n", a)
-
         dp = ev.switch.dp
         dpid = dp.id
 
@@ -236,10 +275,6 @@ class Balancer(app_manager.RyuApp):
     def _link_add_handler(self, ev):
         """
         """
-#         self.logger.debug("memebers of %s: ", ev.link.src)
-#         for a in inspect.getmembers(ev.link.src):
-#             self.logger.debug("\t %s\n", a)
-
         s1 = ev.link.src
         s2 = ev.link.dst
 
@@ -253,8 +288,6 @@ class Balancer(app_manager.RyuApp):
 
     @set_ev_cls(topo_event.EventLinkDelete, MAIN_DISPATCHER)
     def _link_delete_handler(self, ev):
-        """
-        """
         s1 = ev.link.src
         s2 = ev.link.dst
 
@@ -266,6 +299,8 @@ class Balancer(app_manager.RyuApp):
                          self.G.number_of_edges())
 
 
-# TODO: the cache of the Mapper should be cleaned from time to time....
+# TODO: the cache of the Mapper should be cleaned periodically
 # TODO: document your code...
-# TODO: use idle and hard timeout instead of adding thing permanently
+# TODO: use idle and hard timeout instead of adding paths permanently
+# TODO: consider using transport layer header for load balancing
+# TODO: consider using a Weighted graph
